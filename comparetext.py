1e6e8e1b246fb56baac229d600fb8bb95d21cb64
Michael F. Schönitzer <michael@schoenitzer.de>
2015-12-09 15:17:03 +0100

    apply regex to every single word

--- comparetext.py
+++ comparetext.py
@@ -4,9 +4,10 @@ from collections import Counter
 import re
 import sys
 
+#@profile
 def comp():
     common_words = open('de-commonwordlist.txt', 'rU').read().split()
-    re_filters = ['\W+\d+\W+', '\W+\w\W+', '(\W+[A-Z][a-z]{1,2})+\W+' ]
+    re_filters = ['\d+$', '\w$', '[A-Z][a-z]{1,2}$' ]
     
     specialchar_filter = re.compile('[^\w\s]+', re.UNICODE)
     compiled_re_filters = (re.compile(i) for i in re_filters)
@@ -16,13 +17,25 @@ def comp():
     
     txt_1=specialchar_filter.sub('', txt_1)
     txt_2=specialchar_filter.sub('', txt_2)
+    
+    dict_1=Counter(txt_1.split())
+    dict_2=Counter(txt_2.split())
+
     for re_filter in compiled_re_filters:
-        txt_1=re_filter.sub(' ', txt_1)
-        txt_2=re_filter.sub(' ', txt_2)
-    
-    dict_1=Counter(txt_1.lower().split())
-    dict_2=Counter(txt_2.lower().split())
-    
+        for word in list(dict_1):
+            if re_filter.match(word):
+                del dict_1[word]
+        for word in list(dict_2):
+            if re_filter.match(word):
+                del dict_2[word]
+            
+    for word in list(dict_2):
+        dict_2[word.lower()]=dict_2.pop(word)
+            
+    for word in list(dict_1):
+        dict_1[word.lower()]=dict_1.pop(word)
+   
+
     for word in common_words:
         del dict_1[word]
         del dict_2[word]
@@ -39,7 +52,8 @@ def comp():
     print(sp, norm_1, norm_2)
     print(sp/(norm_1*norm_2))
 
-comp()
+for i in range(30):
+    comp()
 
 # Note:
 # Div through zero if one txt is empty
